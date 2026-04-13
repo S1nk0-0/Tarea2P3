72b2ea6cc54e026c34f20a9cb796bdc50c43dfab
fvale <ferrel.infante@utec.edu.pe>
2026-04-12 20:12:26 -0500

    core

--- core_numeric.h
+++ core_numeric.h
@@ -1,9 +1,27 @@
-//
-// Created by fvale on 4/12/2026.
-//
-
 #ifndef TAREA2_CORE_NUMERIC_H
 #define TAREA2_CORE_NUMERIC_H
 
-#endif //TAREA2_CORE_NUMERIC_H
 
+# include < concepts >
+# include < iterator >
+using namespace std;
+
+template < typename C >
+concept Iterable = requires ( C c ) {
+    begin (c) ;
+    end (c) ;
+};
+
+template < typename T >
+concept Addable = requires ( T a , T b ) {
+    { a + b } -> same_as <T >;
+};
+
+template < typename T >
+concept Divisible = requires ( T a , size_t n ) {
+    { a / n } -> same_as <T >;
+};
+// TAREA: Crear uno adicional y aplicarlo
+
+
+#endif //TAREA2_CORE_NUMERIC_H
